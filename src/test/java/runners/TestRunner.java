6bd67a1b916f47f5e312ab2819e99ca9bfc32249
feyza-unal <feyzaunal95@gmail.com>
2023-03-11 21:22:06 +0300

    cucumber

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",//features folder path
         glue = {"stepdefinitions"},   //stepdefinitions path
         tags = "@testrunner",
-        dryRun = false
+        dryRun = true
 
 )
 public class TestRunner {
